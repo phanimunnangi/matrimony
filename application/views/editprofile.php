1865b745ef59d748d5e3007c7b29bba22aa68475
phanimunnangi <munnangi.phani@gmail.com>
2021-12-12 15:27:46 +0530

    Update editprofile.php

--- application/views/editprofile.php
+++ application/views/editprofile.php
@@ -1094,10 +1094,7 @@
 	<!-- Main Container Ends -->
 <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js" type="text/javascript"></script>
 <script>
-	$('.payment_check').click(function(){
-		if($(this).val() == 'no') $('#trans_number').removeAttr('required');
-		if($(this).val() == 'yes') $('#trans_number').addAttr('required');
-	});
+	
 	function viewprofile(){
 		$("#user_image_modal").modal('show');
 	}
@@ -1236,7 +1233,9 @@
 	$('.payment_check').click(function(){
 		if($(this).val() == 'yes'){
 			$('#trans_box').show();
+			$('#trans_number').addAttr('required');
 		}else{
+			$('#trans_number').removeAttr('required');
 			$('#trans_box').hide();
 		}
 	});
